1ac1f9bad0696d7dc918f0f098822b40c2cc9eee
Brady Kurtz <bradykurtz@churchofjesuschrist.org>
2020-10-05 18:35:13 -0600

    Adding instructions

--- src/main/java/edu/weber/interviewQuestion/MultipleService.java
+++ src/main/java/edu/weber/interviewQuestion/MultipleService.java
@@ -2,6 +2,8 @@ package edu.weber.interviewQuestion;
 
 public class MultipleService {
 
+    // Finish this method.  This method should return the product of two numbers
+    // You need to finish this method without using any multiplication operator or function
     public Double multiply(Double d1, Double d2) {
         return null;
     }
