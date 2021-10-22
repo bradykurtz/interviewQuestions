30e9cd612d70841bdef803cf52b61e6f18a0a0b1
Brady Kurtz <bradykurtz@churchofjesuschrist.org>
2021-10-22 11:38:39 -0600

    Adding some comments

--- src/main/java/edu/weber/interviewQuestion/roman/RomanNumeralService.java
+++ src/main/java/edu/weber/interviewQuestion/roman/RomanNumeralService.java
@@ -2,6 +2,13 @@ package edu.weber.interviewQuestion.roman;
 
 public class RomanNumeralService {
 
+    /*
+    Adding Romain Numerals Rule
+    if current number is less than the next number you subtract
+    else if number is equal or greater you add
+    Example: IV = 4
+    Example: XIX = 19
+     */
     public int convert(String romanNumeral) {
 
 
